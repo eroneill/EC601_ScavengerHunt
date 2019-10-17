96a60a8d58131cb763b40ec15164472eec91c378
eroneill <e.12.oneill@gmail.com>
2019-10-17 15:36:41 -0400

    updated untracked files

--- django_poll/mysite/mysite/urls.py
+++ django_poll/mysite/mysite/urls.py
@@ -18,5 +18,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('polls/', include('polls.urls')),
+    path('hunts/', include('hunts.urls')),
     path('admin/', admin.site.urls),
 ]
